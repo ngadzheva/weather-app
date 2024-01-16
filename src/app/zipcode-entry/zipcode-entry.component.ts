ec090f043097d60987794d1929163c9aed801b9c
ngadzheva <nmgadzheva@gmail.com>
2024-01-16 08:35:36 +0200

    Initial commit

---
+++ src/app/zipcode-entry/zipcode-entry.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {LocationService} from "../location.service";
+
+@Component({
+  selector: 'app-zipcode-entry',
+  templateUrl: './zipcode-entry.component.html'
+})
+export class ZipcodeEntryComponent {
+
+  constructor(private locationService : LocationService) { }
+
+  addLocation(zipcode : string){
+    this.locationService.addLocation(zipcode);
+  }
+
+}
